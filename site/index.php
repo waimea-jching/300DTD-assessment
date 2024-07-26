90fa0758b90e7b64d83706410ed493f939f3678e
Josiah <jching@waimea.school.nz>
2024-07-26 15:10:01 +1200

    admin login

--- site/index.php
+++ site/index.php
@@ -8,7 +8,7 @@ require_once 'lib/router.php';
 
 //-------------------------------------------------------------
 // Site Configuration
-const SITE_NAME  = 'PHP Routing with HTMX';
+const SITE_NAME  = 'The Librarys Mystery';
 const SITE_OWNER = 'Waimea College';
 
 
@@ -21,8 +21,9 @@ $router = new Router(['debug' => true]);
 // Define routes
 
 $router->route(GET, PAGE, '/', 'pages/welcome.php', 'layouts/welcome_layout.php');
-$router->route(GET, PAGE, '/intro', 'pages/introduction.php');
-$router->route(GET, PAGE, '/adminLogin', 'pages/adminLogin.php');
+$router->route(GET, PAGE, '/intro', 'pages/introduction.php', 'layouts/hero.php');
+$router->route(GET, PAGE, '/adminLogin', 'pages/adminLogin.php', 'layouts/admin_layout.php');
+$router->route(GET, PAGE, '/validateKey', 'actions/validate-key.php');
 
 
 //-------------------------------------------------------------
